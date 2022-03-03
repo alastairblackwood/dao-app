48fe54b3e6631d2870720834ab742567b66aa847
alastairblackwood <agrizzlybear@icloud.com>
2022-03-03 13:46:44 +1000

    file clean up

--- client/src/components/auth/AuthStatus.tsx
+++ client/src/components/auth/AuthStatus.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import react from 'react';
 import { useNavigate } from 'react-router-dom';
-import { AuthContext } from '../../../contexts/AuthContext';
+import { AuthContext } from '../../contexts/AuthContext';
 
 const useAuth = () => {
   return useContext(AuthContext);
